cd4652146c240fdb57b84149e46616a9c05d744f
jfrey <nomhad@gmail.com>
2014-08-22 14:50:37 +0200

    Put correct code for arduino due.

--- arduino/PulseSensorDue/PulseSensorDue.ino
+++ arduino/PulseSensorDue/PulseSensorDue.ino
@@ -1,50 +1,22 @@
 
-
-
 //  VARIABLES
 int pulsePin = 0;                 // Pulse Sensor purple wire connected to analog pin 0
-int blinkPin = 13;                // pin to blink led at each beat
-int fadePin = 5;                  // pin to do fancy classy fading blink at each beat
-int fadeRate = 0;                 // used to fade LED on with PWM on fadePin
-
 
 // these variables are volatile because they are used during the interrupt service routine!
-volatile int BPM;                   // used to hold the pulse rate
 volatile int Signal;                // holds the incoming raw data
-volatile int IBI = 600;             // holds the time between beats, must be seeded!
-volatile boolean Pulse = false;     // true when pulse wave is high, false when it's low
-volatile boolean QS = false;        // becomes true when Arduoino finds a beat.
-
 
 void setup() {
+  // pulse sensor works best with 3.3V supply, may need the following line depending on your arduino model.
   //analogReference(EXTERNAL);
-  //pinMode(blinkPin, OUTPUT);        // pin that will blink to your heartbeat!
-  //pinMode(fadePin, OUTPUT);         // pin that will fade to your heartbeat!
+  // unleash Due power!
+  analogReadResolution(12);
   Serial.begin(115200);             // we agree to talk fast!
-
-  //pinMode(pulsePin, INPUT);
 }
-
-
 
 void loop() {
-  int Signal = analogRead(pulsePin);
-
-  sendDataToProcessing('S', Signal);     // send Processing the raw Pulse Sensor data
-
-  delay(20);                             //  take a break
+  Signal = analogRead(pulsePin);
+  // the data to send culminating in a carriage return
+  Serial.println(Signal);
+  // simulating a 500hz recordings
+  delay(2);                             //  take a break
 }
-
-
-void sendDataToProcessing(char symbol, int data ) {
-  Serial.print(symbol);                // symbol prefix tells Processing what type of data is coming
-  Serial.println(data);                // the data to send culminating in a carriage return
-//Serial.println("Salut");
-}
-
-
-
-
-
-
-
